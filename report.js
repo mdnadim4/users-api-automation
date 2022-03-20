1d9326e34d9409ddf33579121cb970d1f7857f88
Nadim Mahmud <mdnadim4@gmail.com>
2022-03-21 04:17:40 +0600

    Added Users API Tesing Project

---
+++ report.js
@@ -0,0 +1,21 @@
+const newman = require('newman')
+
+newman.run(
+	{
+		collection: require('./Users_API_Collection.json'),
+		environment: require('./Users_API_Environment.json'),
+		iterationCount: 1,
+		reporters: 'htmlextra',
+		reporter: {
+			htmlextra: {
+				export: './Reports/report.html',
+			},
+		},
+	},
+	function (err) {
+		if (err) {
+			throw err
+		}
+		console.log('Collection run successfully completed!')
+	}
+)
